bcca32a2d1c40dcdcd314cadb8b5fb8f9bcf071d
AnkitaS1996 <rohitshedage60@gmail.com>
2021-02-27 22:41:14 +0530

    Program 1

---
+++ Assignment 4 Array C Program/01 Create Array Without Initilization & Display All Elements.c
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+///  Statement = Create Array Without Initilization And Display All Elements
+///////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int iArr[10];
+    int i = 0;
+
+    printf("\n Display All Elements : ");
+
+    for(i = 0;i <= 10;i++)
+    {
+        printf("\n\t\t\t %d",iArr[i]);
+    }
+}
